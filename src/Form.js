29b7db7ee5c083d2deae890b2caf90dae6ce44c7
donymyounglee <pajang1515@daum.net>
2023-10-05 11:51:17 +0900

    init

---
+++ src/Form.js
@@ -0,0 +1,59 @@
+import {useState} from "react";
+import axios from "axios";
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    description: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Submitted Data:', formData);
+
+    axios.post('http://localhost:8001/api/v1/demo', {
+      name : formData.name,
+      description : formData.description
+    }).then((res) => {
+      if (res.status == 200) {
+        alert("전송성공");
+      }
+    }).catch((err) => {
+      console.log(err);
+      alert("전송실패");
+    })
+
+
+  };
+
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">이름:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="description">설명:</label>
+        <textarea
+          id="description"
+          name="description"
+          value={formData.description}
+          onChange={handleInputChange}
+        />
+        <button type="submit">제출</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
